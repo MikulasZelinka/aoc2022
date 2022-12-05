4bb876cd46fce18c3dda3592eaf6f3e9f7e12987
Mikuláš Zelinka <mikulas@zelinka.dev>
2022-12-05 23:25:53 +0100

    day 04, day 05

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -239,9 +239,117 @@ fn p03(part_two: bool) {
     }
 }
 
+fn p04(part_two: bool) {
+    fn split(x: &str, c: char) -> (&str, &str) {
+        let mut split = x.split(c);
+        (split.next().unwrap(), split.next().unwrap())
+    }
+
+    fn parse(x: &str) -> u32 {
+        x.parse::<u32>().unwrap()
+    }
+
+    fn split_parse(x: &str, c: char) -> (u32, u32) {
+        let (x1, x2) = split(x, c);
+        (parse(x1), parse(x2))
+    }
+    let mut contained: u32 = 0;
+
+    if let Ok(lines) = read_lines("assets/04.txt") {
+        for line in lines {
+            if let Ok(line) = line {
+                if line.is_empty() {
+                    continue;
+                }
+                // println!("{line}");
+
+                let (left, right) = split(&line, ',');
+
+                let (x1, x2) = split_parse(left, '-');
+                let (y1, y2) = split_parse(right, '-');
+
+                if part_two {
+                    if !(x2 < y1 || y2 < x1) {
+                        contained += 1;
+                    }
+                } else {
+                    if (x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2) {
+                        contained += 1;
+                    }
+                }
+            }
+        }
+        println!("{}", contained);
+    } else {
+        println!("error reading file");
+    }
+}
+
+fn p05(part_two: bool) {
+    //             [J]             [B] [W]
+    //             [T]     [W] [F] [R] [Z]
+    //         [Q] [M]     [J] [R] [W] [H]
+    //     [F] [L] [P]     [R] [N] [Z] [G]
+    // [F] [M] [S] [Q]     [M] [P] [S] [C]
+    // [L] [V] [R] [V] [W] [P] [C] [P] [J]
+    // [M] [Z] [V] [S] [S] [V] [Q] [H] [M]
+    // [W] [B] [H] [F] [L] [F] [J] [V] [B]
+    //  1   2   3   4   5   6   7   8   9
+
+    const NUM_STACKS: usize = 9;
+    const NUM_LINES: usize = 8;
+
+    let mut stacks: Vec<VecDeque<char>> = vec![VecDeque::new(); NUM_STACKS + 1];
+    if let Ok(mut lines) = read_lines("assets/05.txt") {
+        for _ in 1..=NUM_LINES {
+            let chars: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
+            // println!("{:?}", chars);
+            for i in 0..NUM_STACKS {
+                let c = chars[1 + 4 * i];
+                if c.is_alphabetic() {
+                    stacks[i + 1].push_back(c);
+                }
+            }
+        }
+        // println!("Start:\n{:?}", stacks);
+
+        for line in lines {
+            if let Ok(line) = line {
+                let words: Vec<&str> = line.split(' ').collect();
+                if words.len() != 6 {
+                    continue;
+                }
+                let [how_many, from, to]: [usize; 3] =
+                    [words[1], words[3], words[5]].map(|x| x.parse().unwrap());
+
+                for i in 0..how_many {
+                    let c: char;
+                    if part_two {
+                        c = stacks[from].remove(how_many - i - 1).unwrap();
+                    } else {
+                        c = stacks[from].pop_front().unwrap();
+                    }
+                    stacks[to].push_front(c);
+                }
+            }
+        }
+
+        // println!("Finish:\n{:?}", stacks);
+
+        for i in 1..=NUM_STACKS {
+            print!("{}", stacks[i].pop_front().unwrap());
+        }
+        println!();
+    }
+}
+
 fn main() {
     println!("Hello, advent!");
 
+    p05(true);
+    p05(false);
+    p04(true);
+    p04(false);
     p03(true);
     p03(false);
     p02(true);
